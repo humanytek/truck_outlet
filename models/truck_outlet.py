ae7d72ac5e7b7ed4797b8d5861ad82aa665dabd2
AfroMonkey <moisalejandro@gmail.com>
2016-09-19 11:51:54 -0500

    [IMP]

--- models/truck_outlet.py
+++ models/truck_outlet.py
@@ -8,7 +8,7 @@ class TruckOutlet(models.Model):
     state = fields.Selection([
         ('analysis', 'Analysis'),
         ('weight_input', 'Weight Input'),
-        ('unloading', 'Unloading'),
+        ('loading', 'Loading'),
         ('weight_output', 'Weight Output'),
         ('done', 'Done'),
     ], default='analysis')
@@ -16,12 +16,17 @@ class TruckOutlet(models.Model):
     _defaults = {'name': lambda obj, cr, uid, context: obj.pool.get('ir.sequence').get(cr, uid, 'reg_code'), }
 
     @api.one
+    @api.depends('input_kilos', 'output_kilos')
+    def _compute_raw_kilos(self):
+        self.raw_kilos = self.output_kilos -self.input_kilos
+
+    @api.one
     @api.depends('contract_id', 'clean_kilos')
     def _compute_delivered(self):
         self.delivered = sum(record.clean_kilos for record in self.contract_id.truck_outlet_ids) / 1000
 
     @api.one
-    def fun_unload(self):
+    def fun_load(self):
         self.state = 'weight_output'
 
     @api.multi
@@ -30,8 +35,8 @@ class TruckOutlet(models.Model):
             if self.state == 'analysis':
                 self.write({'state': 'weight_input'}, 'r')
             elif self.state == 'weight_input':
-                self.write({'state': 'unloading'}, 'r')
-            elif self.state == 'unloading':
+                self.write({'state': 'loading'}, 'r')
+            elif self.state == 'loading':
                 self.write({'state': 'weight_output'}, 'r')
             elif self.state == 'weight_output':
                 self.write({'state': 'done'}, 'r')
